2456d823319d64a7750ac5b7bcae109f70122838
Alistair Ramsay <alistair@digitalpath.co.uk>
2017-09-06 17:55:47 +0100

    Edits

--- config.rb
+++ config.rb
@@ -6,9 +6,10 @@ css_dir = "css"
 sass_dir = "css"
 images_dir = "img"
 javascripts_dir = "js"
+output_style = :compressed
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+#output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
